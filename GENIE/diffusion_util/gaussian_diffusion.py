3fb0ed8f376e587999a37bb9e3ad18536bba63cb
umango101 <53319579+umango101@users.noreply.github.com>
2025-07-21 01:54:59 -0400

    Update gaussian_diffusion.py

--- GENIE/diffusion_util/gaussian_diffusion.py
+++ GENIE/diffusion_util/gaussian_diffusion.py
@@ -1714,7 +1714,7 @@ class GaussianDiffusion:
         if noise is None:
             noise = th.randn_like(x_start)
         x_t = self.q_sample(x_start, t, noise=noise)  # reparametrization trick.
-        get_logits = model.module.get_logits
+        get_logits = model.get_logits
 
         terms = {}
 
